a62fbec6a22cf6d3fdfcf0c1fbc2b12a408a93d7
hs <sibbengaharry@gmail.com>
2021-08-20 11:21:53 +0100

    Created logo component added images to assets

---
+++ components/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-img
+    :src="require('../assets/images/Torque_white_logo.png')"
+    alt="Torque Motorsport logo"
+    :max-width="width"
+    :max-height="height"
+  ></v-img>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: Number,
+      required: true,
+    },
+    height: {
+      type: Number,
+      required: true,
+    },
+  },
+}
+</script>
