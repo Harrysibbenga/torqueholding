098ba4a6a3ebea16f8cd94b96bf0226cc2febbaa
hs <sibbengaharry@gmail.com>
2021-08-20 11:22:38 +0100

    Created social icons component

---
+++ components/app/socialIcons.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row justify="end">
+    <v-icon
+
+      color="white"
+    >
+      mdi-twitter
+    </v-icon>
+
+    <v-icon
+
+      color="white"
+    >
+      mdi-facebook
+    </v-icon>
+
+    <v-icon
+
+      color="white"
+    >
+      mdi-instagram
+    </v-icon>
+
+    <v-icon
+
+      color="white"
+    >
+      mdi-linkedin
+    </v-icon>
+  </v-row>
+</template>
